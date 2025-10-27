56a8be18f9a731d9de15826191fe6e7e4739ae89
LukeBoncic <48628090+LukeBoncic@users.noreply.github.com>
2025-10-27 11:46:34 -0400

    Fix assert function

--- debug.h
+++ debug.h
@@ -10,7 +10,8 @@
 #ifndef DEBUG
 #define assert(e)
 #else
-#define assert(e) { if (!e) print("\n\nAssertion Failed [ File %s: Line %u ]", __FILE__, __LINE__); while (1); }
+#define assert(e) { if (!(e)) print("\n\nAssertion Failed [ File %s: Line %u ]", __FILE__, __LINE__); while (1); }
 #endif
 
 #endif
+
