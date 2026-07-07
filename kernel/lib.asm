5120d99e9b1726688b6a396e10ba2ec37d2766af
Luke Boncic <lboncicgd@gmail.com>
2026-07-06 20:18:40 -0400

    Change from spaces to tabs

--- kernel/lib.asm
+++ kernel/lib.asm
@@ -5,40 +5,40 @@ global memmove
 global memcmp
 
 memset:
-    cld
-    mov ecx,edx
-    mov al,sil
-    rep stosb
-    ret
+	cld
+	mov ecx,edx
+	mov al,sil
+	rep stosb
+	ret
 
 memcmp:
-    cld
-    xor eax,eax
-    mov ecx,edx
-    repe cmpsb
-    setnz al
-    ret
+	cld
+	xor eax,eax
+	mov ecx,edx
+	repe cmpsb
+	setnz al
+	ret
 
 memcpy:
 memmove:
-    cld
-    cmp rsi,rdi
-    jae .copy
-    mov r8,rsi
-    add r8,rdx
-    cmp r8,rdi
-    jbe .copy
+	cld
+	cmp rsi,rdi
+	jae .copy
+	mov r8,rsi
+	add r8,rdx
+	cmp r8,rdi
+	jbe .copy
 
 .overlap:
-    std
-    add rdi,rdx
-    add rsi,rdx
-    sub rdi,1
-    sub rsi,1
+	std
+	add rdi,rdx
+	add rsi,rdx
+	sub rdi,1
+	sub rsi,1
 
 .copy:
-    mov ecx,edx
-    rep movsb
-    cld
-    ret
-    
+	mov ecx,edx
+	rep movsb
+	cld
+	ret
+	
--- kernel/trap.asm
+++ kernel/trap.asm
@@ -35,205 +35,205 @@ global TrapReturn
 global in_byte
 
 Trap:
-    push rax
-    push rbx  
-    push rcx
-    push rdx  	  
-    push rsi
-    push rdi
-    push rbp
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
+	push rax
+	push rbx  
+	push rcx
+	push rdx  	  
+	push rsi
+	push rdi
+	push rbp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
 
-    mov rdi,rsp
-    call handler
+	mov rdi,rsp
+	call handler
 
 TrapReturn:
-    pop	r15
-    pop	r14
-    pop	r13
-    pop	r12
-    pop	r11
-    pop	r10
-    pop	r9
-    pop	r8
-    pop	rbp
-    pop	rdi
-    pop	rsi  
-    pop	rdx
-    pop	rcx
-    pop	rbx
-    pop	rax       
+	pop	r15
+	pop	r14
+	pop	r13
+	pop	r12
+	pop	r11
+	pop	r10
+	pop	r9
+	pop	r8
+	pop	rbp
+	pop	rdi
+	pop	rsi  
+	pop	rdx
+	pop	rcx
+	pop	rbx
+	pop	rax	   
 
-    add rsp,16
-    iretq
+	add rsp,16
+	iretq
 
 
 
 vector0:
-    push 0
-    push 0
-    jmp Trap
+	push 0
+	push 0
+	jmp Trap
 
 vector1:
-    push 0
-    push 1
-    jmp Trap
+	push 0
+	push 1
+	jmp Trap
 
 vector2:
-    push 0
-    push 2
-    jmp Trap
+	push 0
+	push 2
+	jmp Trap
 
 vector3:
-    push 0
-    push 3	
-    jmp Trap 
+	push 0
+	push 3	
+	jmp Trap 
 
 vector4:
-    push 0
-    push 4	
-    jmp Trap   
+	push 0
+	push 4	
+	jmp Trap   
 
 vector5:
-    push 0
-    push 5
-    jmp Trap    
+	push 0
+	push 5
+	jmp Trap	
 
 vector6:
-    push 0
-    push 6	
-    jmp Trap      
+	push 0
+	push 6	
+	jmp Trap	  
 
 vector7:
-    push 0
-    push 7	
-    jmp Trap  
+	push 0
+	push 7	
+	jmp Trap  
 
 vector8:
-    push 8
-    jmp Trap  
+	push 8
+	jmp Trap  
 
 vector10:
-    push 10	
-    jmp Trap 
-                   
+	push 10	
+	jmp Trap 
+				   
 vector11:
-    push 11	
-    jmp Trap
-    
+	push 11	
+	jmp Trap
+	
 vector12:
-    push 12	
-    jmp Trap          
-          
+	push 12	
+	jmp Trap		  
+		  
 vector13:
-    push 13	
-    jmp Trap
-    
+	push 13	
+	jmp Trap
+	
 vector14:
-    push 14	
-    jmp Trap 
+	push 14	
+	jmp Trap 
 
 vector16:
-    push 0
-    push 16	
-    jmp Trap          
-          
+	push 0
+	push 16	
+	jmp Trap		  
+		  
 vector17:
-    push 17	
-    jmp Trap                         
-                                                          
+	push 17	
+	jmp Trap						 
+														  
 vector18:
-    push 0
-    push 18	
-    jmp Trap 
-                   
+	push 0
+	push 18	
+	jmp Trap 
+				   
 vector19:
-    push 0
-    push 19	
-    jmp Trap
+	push 0
+	push 19	
+	jmp Trap
 
 vector32:
-    push 0
-    push 32
-    jmp Trap
+	push 0
+	push 32
+	jmp Trap
 
 vector33:
-    push 0
-    push 33
-    jmp Trap
+	push 0
+	push 33
+	jmp Trap
 
 vector39:
-    push 0
-    push 39
-    jmp Trap
+	push 0
+	push 39
+	jmp Trap
 
 sysint:
-    push 0
-    push 0x80
-    jmp Trap
+	push 0
+	push 0x80
+	jmp Trap
 
 eoi:
-    mov al,0x20
-    out 0x20,al
-    ret
+	mov al,0x20
+	out 0x20,al
+	ret
 
 read_isr:
-    mov al,11
-    out 0x20,al
-    in al,0x20
-    ret
+	mov al,11
+	out 0x20,al
+	in al,0x20
+	ret
 
 load_idt:
-    lidt [rdi]
-    ret
+	lidt [rdi]
+	ret
 
 load_cr3:
-    mov rax,rdi
-    mov cr3,rax
-    ret
+	mov rax,rdi
+	mov cr3,rax
+	ret
 
 read_cr2:
-    mov rax,cr2
-    ret
+	mov rax,cr2
+	ret
 
 read_cr3:
-    mov rax,cr3
-    ret
+	mov rax,cr3
+	ret
 
 pstart:
-    mov rsp,rdi
-    jmp TrapReturn
+	mov rsp,rdi
+	jmp TrapReturn
 
 swap:
-    push rbx
-    push rbp
-    push r12
-    push r13
-    push r14
-    push r15
-    
-    mov [rdi],rsp
-    mov rsp,rsi
-    
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop rbp
-    pop rbx
-    
-    ret 
+	push rbx
+	push rbp
+	push r12
+	push r13
+	push r14
+	push r15
+	
+	mov [rdi],rsp
+	mov rsp,rsi
+	
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbp
+	pop rbx
+	
+	ret 
 
 in_byte:
-    mov rdx,rdi
-    in al,dx
-    ret   
+	mov rdx,rdi
+	in al,dx
+	ret   
 
 
