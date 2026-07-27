e18c4b0968f75dbe644251198b2d139a390a3b8d
Luke Boncic <lboncicgd@gmail.com>
2026-07-27 00:56:33 -0400

    Rewrite boot/debug.c

--- boot/debug.c
+++ boot/debug.c
@@ -1,8 +1,11 @@
 #include "debug.h"
 #include "print.h"
 
+// This function is called when an assertion fails and
+// shows the file and line that caused the failure
+
 void error_check(char *file, uint64_t line)
 {
-	printk("Assertion Failed [%s:%u]", file, line);
+	printk("Assertion Failed [file %s: line %u]", file, line);
 	while (1) { }
 }
